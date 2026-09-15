2418931e910c1c60afdc0c28322941427e94d1ef
ahoorarash9-droid <ahoorarash9@gmail.com>
2026-09-15 13:06:31 +0330

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://pwvoczlmdpicylsdvmcb.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_6bGXzw896-R0Qjgq1jxNOQ_ADV-P1_1";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
